e6f6d8765d7163628190fccb302340dff66a9d97
ismailrascic <ismaily.rascic@gmail.com>
2021-03-30 00:55:06 +0200

    react

---
+++ ex02/src/Table.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+ class Table extends Component{
+render(){
+return(
+<table>
+<thead>
+<tr>
+<th>Firts name</th>
+<th>Last name</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+    <td>Lary</td>
+     <td>Pege</td>
+</tr>
+<tr>
+    <td>Ada</td>
+     <td>Lovelace</td>
+</tr>
+<tr>
+    <td>Alan</td>
+     <td>Turing</td>
+</tr>
+</tbody>
+</table>
+)
+}
+ }
+ export default Table;
---
+++ ex07/src/Box.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+const Box = () =>{
+ return(
+   
+  <h2>This is a Box Component!</h2>
+
+)
+
+}
+export default Box ;
